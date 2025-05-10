ffdc19d4e163658dd8de6200cf16b0ba55c06de0
AdrianPhub <camiloryt89@gmail.com>
2025-05-09 23:28:00 -0500

    Update loginService.js

--- js/loginService.js
+++ js/loginService.js
@@ -1,22 +1,56 @@
-document.getElementById("loginForm").addEventListener("submit", function(e) {
+document.getElementById("formLogin").addEventListener('submit', function (e) {
+    e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    console.log('valores leídos del formulario', {email, password})
-    Login(email, password)
+
+    login(email,password)
 })
 
-function Login(email, password) {
-    let message=''
-    let alertType=''
-    const LOGIN_EDNDPOINT = 'https://reqres.in/api/login'
-    fetch()
-    if()email === '' || password === '') {  
-    alertType = 'warning'
-    message = 'complete los datos'
+function login(email, password) {
+    let message = ''
+    let alertType = ''
+    localStorage.removeItem('token')
+    fetch("https://reqres.in/api/login", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            'x-api-key': 'reqres-free-v1'
+        }, body: JSON.stringify({ email, password }),
+    })
+        .then((response) => {
+            if(response.status == 200){
+                alertType = 'success'
+                message = 'Inicio de sesion exitoso';
+                console.log('responde bien' + response)
+                alertBuilder(alertType, message)
+                localStorage.setItem('token', response.token)
+                setTimeout(() => {
+                    location.href = 'admin/dashboard.html'
+                }, 2000) // 2000ms = 2 seg
+                
 
-    }
-    else if (email === 'prueba gamil@.com'&& password === '123456') {
-        alertType = 'success'
-        message = 'Complete los datos'
-    }
-    .then((result))
+            }else{
+                alertType = 'danger'
+                message = 'Correo o contraseña incorrecto'
+                alertBuilder(alertType, message)
+            }
+            
+        })
+        .catch((error) => {
+            alertType = 'danger'
+            message = 'Error inesperdo'
+            console.error(error)
+            alertBuilder(alertType, message)
+        })
+    
+}
+
+function alertBuilder(alertType, message){
+    const alert = `
+            <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
+            ${message}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        `;
+    document.getElementById('alert').innerHTML = alert;
+}
